06847650035761492f46d64cfdd4459ed62e8e83
Henry C <henrybclarke@protonmail.com>
2022-05-26 21:13:52 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Henry's Mini Jam #107 game
+## Title Pending
+Alright guys, my first game jam! What to make what to make? Only time will tell I suppose. I wish all of my fellow contestants good luck in their projects!
+
+Theme: Music
+Limitation: Gravity is under your control
---
+++ main.lua
@@ -0,0 +1,37 @@
+--[[
+        Main game file for minijam game.
+        Copyright (C) 2022  Henry B. Clarke
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU Affero General Public License as
+        published by the Free Software Foundation, either version 3 of the
+        License, or (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU Affero General Public License for more details.
+
+        You should have received a copy of the GNU Affero General Public License
+        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+        --- [END DISCLAIMER] ---
+
+        Theme: Music
+        Limitation: Gravity is in your hands (wow that's a wierd combination.)
+]]--
+
+
+-- LOVEly boilerplate
+
+function love.load()
+    love.window.setTitle("MiniJam #107 game (title pending) -- v0.0.1")
+end
+
+function love.draw()
+    
+end
+
+function love.update(dt)
+    
+end
